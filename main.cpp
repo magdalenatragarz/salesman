0059c1df37e9b2d3449f9fd9956daf4a19a47fd5
anapneo <magdtrag@gmail.com>
2020-03-21 13:46:05 +0100

    Removed googletest submodule, added SFML

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <evolution/map.hpp>
-#include <evolution/salesman_algorithm.hpp>
+//#include <evolution/map.hpp>
+//#include <evolution/salesman_algorithm.hpp>
 
 #include <iostream>
 #include <memory>
@@ -8,18 +8,20 @@
 
 int main() {
 
-    std::string file_path = "D:\\projekty\\salesman_parser\\15cities.map";
-    auto map = std::make_shared<evolution::map>(file_path);
+        std::cout << "HELLO";
 
-    auto algorithm = std::make_shared<evolution::salesman_algorithm>(map, 50);
-
-    auto start = std::chrono::steady_clock::now();
-    algorithm->run();
-    auto end = std::chrono::steady_clock::now();
-
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns" << std::endl;
-    std::cout << "path: " <<  algorithm->get_best_solution() << std::endl;
-    std::cout << "distance: " << algorithm->get_best_solution()->get_distance() << std::endl;
+//    std::string file_path = "D:\\projekty\\salesman_parser\\15cities.map";
+//    auto map = std::make_shared<evolution::map>(file_path);
+//
+//    auto algorithm = std::make_shared<evolution::salesman_algorithm>(map, 50);
+//
+//    auto start = std::chrono::steady_clock::now();
+//    algorithm->run();
+//    auto end = std::chrono::steady_clock::now();
+//
+//    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns" << std::endl;
+//    std::cout << "path: " <<  algorithm->get_best_solution() << std::endl;
+//    std::cout << "distance: " << algorithm->get_best_solution()->get_distance() << std::endl;
 
 
 }
